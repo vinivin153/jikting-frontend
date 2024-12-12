70f5898f8b3cd0fe94d99f255d75c2c16a747800
vinivin153 <vinivin153@naver.com>
2024-12-12 10:36:06 +0900

    chore: 프록시 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    _global: {},
-  },
-  plugins: [react(), tsconfigPaths(), svgr({ exportAsDefault: true })],
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      _global: {},
+    },
+    plugins: [react(), tsconfigPaths(), svgr({ exportAsDefault: true })],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
